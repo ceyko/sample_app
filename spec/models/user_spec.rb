63dba838d4065d1391ca4a3e5de77997910b66ed
Mike Ceyko <ceykooo@yahoo.com>
2011-03-27 01:09:25 -0400

    Added user email uniqueness in model & db

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -46,4 +46,18 @@ describe User do
       invalid_email_user.should_not be_valid
     end
   end
+
+  it "should reject duplicate email addresses" do
+    # Put a user with given email address into the database
+    User.create!(@attr)
+    user_with_duplicate_email = User.new(@attr)
+    user_with_duplicate_email.should_not be_valid
+  end
+
+  it "should reject case-insensitive duplicate email addresses" do
+    upcase_email = @attr[:email].upcase
+    User.create!(@attr.merge(:email => upcase_email))
+    user_with_duplicate_email = User.new(@attr)
+    user_with_duplicate_email.should_not be_valid
+  end
 end
